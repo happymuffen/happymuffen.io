8b1cdaf19b05004fa268b1f31b6f310587dae874
happymuffen <yakurbe0112@gmail.com>
2019-12-14 04:15:20 -0700

    Added functions for displaying beziar curves
    Made a beziar curve class to handle the creation and functioning of beziar curves. 
    
    Includes a function to split beziar curves into shorter curves on the same path. the math is cool but complected. not quite sure why it works. something about derivatives. I've tested the theory but not my implementation.
    
    Includes a function to make a line of svg code that matches the curve. 
    Tested it. seems to work.

--- animations.js
+++ animations.js
@@ -6,13 +6,51 @@ var lock;
 var mousepos=[0,0];
 var wsize=[window.innerWidth,window.innerHeight];
 
+var svg_element="svg";
 
-function init(){
-	cursor=document.getElementById("cursor");
+class beziar{
+	constructor(p0,p1,p2,p3,color,width){
+		this.curve=[p0,p1,p2,p3];
+		this.color=color;
+		this.width=width;
+	}
+	slice(per0,per1){
+		function midpnt(p0,p1,per){
+			var x=p0[0]+(p1[0]-p0[0])*per;
+			var y=p0[1]+(p1[1]-p0[1])*per;
+			return [x,y];
+		}
+		function split(b0,b1,b2,b3,per){
+			if(per==0) return [new beziar(b0,b0,b0,b0,this.color,this.width),new beziar(b0,b1,b2,b3,this.color,this.width)];
+			if(per==1) return [new beziar(b0,b1,b2,b3),new beziar(b3,b3,b3,b3,this.color,this.width,this.color,this.width)];
+			var l1=midpnt(b0,b1);
+			var m=midpnt(b1,b2);
+			var r2=midpnt(b2,b3);
+			var l2=midpnt(l1,m);
+			var r1=midpnt(m,r2);
+			var splt=midpnt(l2,r1);
+			var l=new beziar(b0,l1,l2,splt);
+			var r=new beziar(splt,r1,r2,b1);
+			return [l,r];
+		}
+		if(per0>1||per0<0||per1>1||per1<0){
+			return null;
+		}
+		var splt=split(this.curve[0],this.curve[1],this.curve[2],this.curve[3],per1);
+		var final=split(splt[1].curve[0],splt[1].curve[1],splt[1].curve[2],splt[1].curve[3],per1*per0);
+		
+		return final[0];
+	}
+	print(){
+		var d="m"+this.curve[0][0]+","+this.curve[0][1]+"c"+this.curve[1][0]+","+this.curve[1][1]+" "+this.curve[2][0]+","+this.curve[2][1]+" "+this.curve[3][0]+","+this.curve[3][1];
+		var out="<path d=\""+d+"\" id=\"asdf\" stroke=\"#"+this.color+"\" stroke-width=\""+this.width+"\" fill=\"none\" stroke-linecap=\"round\">";
+		return out;
+	}
 }
 
+var objs=new beziar([10,20],[30,40],[500,60],[8,90],"45696e",5);
 
-function animate(text,element){
+function animate_text(text,element){
 	
 	lock=text;
 	var len=text.length;
@@ -40,6 +78,17 @@ function animate(text,element){
 	}
 	looper();
 }
+
+function animate_svg(time){
+	
+	var header="<svg width=\""+wsize[0]+"\" height=\""+wsize[1]+"\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><g class=\"layer\"><title>Layer 1</title>\n";
+	var footer='\n</g></svg>';
+	var out=header+objs.print()+footer;
+
+	document.getElementById(svg_element).innerHTML=out;
+	//requestAnimationFrame(animate_svg);
+	
+}
 	
 //~ function mouseloop(time){
 	//~ <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
@@ -54,6 +103,9 @@ function animate(text,element){
 	//~ requestAnimationFrame(mouseloop);
 	//~ debug(mousepos);
 //~ }
+
+
+
 
 function multianimate(text,element){
 	var pars=text.split("\n");
@@ -118,13 +170,12 @@ function contact(){
 }
 
 function debug(text){
-	document.getElementById("debug").innerHTML="<p>"+text;
+	document.getElementById("debug").innerHTML="<p>\\"+text+"";
 }
 
-function mousemove(event){
-	mousepos=[event.clientX,event.clientY];
-}
+//~ function mousemove(event){
+	//~ mousepos=[event.clientX,event.clientY];
+//~ }
 
 
-
-//requestAnimationFrame(mouseloop);
+requestAnimationFrame(animate_svg);
