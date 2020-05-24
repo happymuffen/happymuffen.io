e29e3059513c048377611d8c19472c6c7d75c222
happymuffen <yakurbe0112@gmail.com>
2020-05-24 04:19:48 -0600

    Cleaned up animate_functions
    Improved parser loops in constructor. Attempted to get it printing the svg as background image.

--- animations.js
+++ animations.js
@@ -5,13 +5,38 @@ var speed=1;
 var lock;
 var mousepos=[0,0];
 var wsize=[window.innerWidth,window.innerHeight];
-var butterfly;
 
 const svg_element=document.getElementById("svg");
 
-import {sprite} from './modules/animate_functions.js';
-import {bfjs} from './modules/butterfly.js';
+import {beziar, frame, animation, sprite} from './modules/animate_functions.js';
 
+var adsf=new beziar([50,50],[200,30],[80,20],[60,60],"aaaaaa",5);
+var objs=new beziar([50,50],[200,30],[80,20],[60,60],"45696e",5);
+objs=objs.slice(0,.8);
+var f1=new frame([adsf,objs]);
+var obs2=new beziar([50,50],[200,30],[80,20],[60,60],"45696e",5);
+obs2=obs2.slice(0,.6)
+frames=[];
+for(var i=1;i<=20;i++){
+	var curve=new beziar([50,50],[200,30],[80,20],[60,60],"45696e",5);
+	curve=curve.slice(0,i/20);
+	var f=new frame([adsf,curve])
+	frames.push(f);
+	frames.push(i);
+}
+var ani=new animation(frames);
+var spr=new sprite(ani);
+
+function animate_svg(time){
+	
+	var header="<svg width=\""+wsize[0]+"\" height=\""+wsize[1]+"\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><g class=\"layer\"><title>Layer 1</title>\n";
+	var footer='\n</g></svg>';
+	var out=header+adsf.print()+spr.draw()+footer;
+	//console.log(out);
+	svg_element.innerHTML=out;
+	requestAnimationFrame(animate_svg);
+	
+}
 
 function about(){
 	animate(aboutText,"body");
@@ -37,11 +62,10 @@ function debug(text){
 }
 
 
-function startBf(){
-	const butterfly=new sprite("butterfly");
-	bfjs(butterfly);
-	butterfly.idle();
+function butterfly(){
+	animation.import("butterfly",0);
 }
 
-window.onload=startBf;
+window.onload=butterfly;
+requestAnimationFrame(animate_svg);
 
