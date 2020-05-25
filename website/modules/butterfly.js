739475d91cbaba379cf53661c0d90de3769d176d
happymuffen <yakurbe0112@gmail.com>
2020-05-25 03:04:48 -0600

    Cleaning up
    Removed extra junk added on accedent.

--- website/modules/butterfly.js
+++
@@ -1,8 +0,0 @@
-//functions that will be added to the butterfly sprite object
-
-export function bfjs(bf){
-	//bf.idle=idle;//add idle function
-}
-function idle(){
-	console.log("flap flap flap");
-}
