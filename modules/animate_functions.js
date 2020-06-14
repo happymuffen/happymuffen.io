9c68bbeda807971ec8b3d1b2e8785150c2957dd2
happymuffen <yakurbe0112@gmail.com>
2020-06-14 02:44:30 -0600

    Got basic animations working
    Pulling headers from svg and layer attributes. Making deep copies of frames. Ready to start on additional animations and ai.

--- modules/animate_functions.js
+++ modules/animate_functions.js
@@ -3,7 +3,11 @@ export class sprite{//takes svg file and instruction set
 	constructor(svg){
 		//set svg, relevent svg layers
 		var e=document.getElementById(svg);
-		var raw=e.contentWindow.document.childNodes[1].childNodes;
+		var raw=e.contentWindow.document.childNodes[1];
+		//pull data from headers (viewbox)
+		this.headers=raw.attributes;
+		
+		raw=raw.childNodes;
 		var layers=[];
 		var layernums=[];
 		for(var i=0; i<raw.length; i++){
@@ -16,14 +20,15 @@ export class sprite{//takes svg file and instruction set
 		//split layers into frames
 		var spritesheets=[];
 		var frames=[];
-		this.spritesMap=[]
+		this.layersData=[]
 		for(var i=1; i<layers.length; i++){
 			for(var j=1; j<layers[i]["childNodes"].length; j++){
 				if(layers[i]["childNodes"][j]["childNodes"].length==0)continue;
 				frames.push(layers[i]["childNodes"][j].innerHTML)
 			}
-			this.spritesMap.push(layers[i]["attributes"][2].nodeValue)//Atribute 2 is "inkscape:label"
-			if(this.spritesMap[this.spritesMap.length-1]=="idle")this.idleIndex=spritesheets.length;
+			//LayersData holds all the data avalible in the layer tag. Some is neccecary for proper display (like translate) 
+			this.layersData.push(layers[i]["attributes"])
+			if(this.layersData[this.layersData.length-1][2].nodeValue=="idle")this.idleIndex=spritesheets.length;//Atribute 2 is "inkscape:label"
 			spritesheets.push(frames);
 			frames=[];
 		}
@@ -35,21 +40,34 @@ export class sprite{//takes svg file and instruction set
 		this.x=0;
 		this.y=0;
 		this.stack=[];
+		this.layerIndex=this.idleIndex;
 	}
 	
 	print(svg){
 		//output text for current frame
-		var header="<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><g class=\"layer\"><title>Layer 1</title>\n";
+		var header="<svg "
+		for(var i=0;i<this.headers.length;i++){
+			if(this.headers[i].name=="width" || this.headers[i].name=="height") continue;
+			header+=this.headers[i].name+"=\""+this.headers[i].value+"\"\n";
+		}
+		header+="width=\"100vw\" height=\"100vh\">\n";
 		var footer='\n</g></svg>';
-		var out=header+svg+footer;
+		var layerLabel="<g ";
+		var layerData=this.layersData[this.layerIndex];
+		for(var i=0;i<layerData.length;i++){
+			
+			layerLabel+=layerData[i].name+"=\""+layerData[i].value+"\"\n";
+		}
+		layerLabel+=">";
+		var out=header+layerLabel+svg+footer;
 		return out;
 	}
-	idle(){
-		//default idle. runs through idle frames in sequence and repeats. overwrite with better AI.
-		
-		if(this.stack.length==0){
-			this.stack=this.spriteSheets[this.idleIndex];
-		}
+	idle(){//overwrites animation stack and layerIndex with idle
+		this.stack=JSON.parse(JSON.stringify(this.spriteSheets[this.idleIndex]));//deep copy spritesheet
+		this.layerIndex=this.idleIndex;
+	}
+	move(){//returns next frame in stack. Idles if stack is empty.
+		if(this.stack.length==0) this.idle();
 		var currentFrame=this.stack.shift();
 		return this.print(currentFrame);
 	}
